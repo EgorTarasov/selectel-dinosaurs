b8a3c09322e3b7fe3499eca2fa34ebe48ac8d116
Ilin Ivan <buran05@icloud.com>
2024-02-23 19:16:52 +0300

    serializers (exclude Bank, User)

--- backend/app/api/router/pet.py
+++ backend/app/api/router/pet.py
@@ -1,3 +1,4 @@
+import fastapi
 from fastapi import FastAPI, HTTPException, Depends, Form, APIRouter
 from sqlalchemy.orm import Session
 from fastapi.encoders import jsonable_encoder
@@ -6,6 +7,8 @@ import typing as tp
 
 from ..middlewares.db_session import get_session
 from ..schemas import PetDto, PetCreate, PetDonateAble
+from ..serializers import db_pet_to_pet_dto, db_pets_to_pet_dtos
+from ...crud import get_pet
 
 router: tp.Final[APIRouter] = APIRouter(prefix="/pets")
 
@@ -13,7 +16,10 @@ router: tp.Final[APIRouter] = APIRouter(prefix="/pets")
 # Endpoint to get information about a specific pet including vaccinations, donations, and requests
 @router.get("/pets/{pet_id}", response_model=PetDto)
 def get_pet_info(pet_id: int, db: Session = Depends(get_session)):
-    return PetDto()
+    try:
+        return db_pet_to_pet_dto(await get_pet(pet_id, db))
+    except Exception:
+        raise fastapi.HTTPException(status_code=404, detail="Pet with ")
 
 
 # Endpoint to create a new pet
