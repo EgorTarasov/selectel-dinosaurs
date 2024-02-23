f40ce4db94bd227feee67725dc670aa701c849bd
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2024-02-23 22:20:28 +0300

    wip(backend): pet crud

--- backend/app/crud/pet.py
+++ backend/app/crud/pet.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Session, selectinload
+from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
 import sqlalchemy as sa
 from ..models import Pet
 from typing import Optional, List
@@ -6,7 +7,7 @@ from typing import Optional, List
 from ..api.schemas import PetCreate, PetUpdate
 
 
-async def create_pet(owner_id: int, pet_create: PetCreate, db: Session) -> Pet:
+async def create_pet(owner_id: int, pet_create: PetCreate, db: AsyncSession) -> Pet:
     pet = Pet(
         type=pet_create.type,
         breed=pet_create.breed,
@@ -15,18 +16,24 @@ async def create_pet(owner_id: int, pet_create: PetCreate, db: Session) -> Pet:
         age=pet_create.age,
         weight=pet_create.weight,
         able_to_donate=pet_create.able_to_donate,
-        owner_id=owner_id
+        owner_id=owner_id,
     )
     db.add(pet)
     await db.commit()
-    await db.refresh(pet, ["id", "owner", "vaccines", "blood_donations", "blood_requests"])
+    await db.refresh(
+        pet, ["id", "owner", "vaccines", "blood_donations", "blood_requests"]
+    )
     print("pet", pet)
 
     return pet
 
 
-async def update_pet(owner_id: int, pet_id: int, pet_update: PetUpdate, db: Session) -> Pet:
-    pet = db.query(Pet).filter(Pet.id == pet_id).first()
+async def update_pet(
+    owner_id: int, pet_id: int, pet_update: PetUpdate, db: AsyncSession
+) -> Pet:
+    stmt = sa.select(Pet).filter(Pet.id == pet_id)
+    pet = (await db.execute(stmt)).scalar_one_or_none()
+    # pet = db.query(Pet).filter(Pet.id == pet_id).first()
     if pet:
         if pet.owner_id != owner_id:
             raise ValueError("You are not owner of this pet")
@@ -40,16 +47,23 @@ async def update_pet(owner_id: int, pet_id: int, pet_update: PetUpdate, db: Sess
         raise ValueError(f"Pet with id {pet_id} not found")
 
 
-async def get_pet(pet_id: int, db: Session) -> Pet:
-    pet = db.query(Pet).filter(Pet.id == pet_id).first()
+async def get_pet(pet_id: int, db: AsyncSession) -> Pet:
+
+    stmt = sa.select(Pet).filter(Pet.id == pet_id)
+    pet = (await db.execute(stmt)).scalar_one_or_none()
     if pet:
         return pet
     raise ValueError(f"Pet with id {pet_id} not found")
 
 
-async def get_pets(db: Session) -> List[Pet]:
-    stmt = sa.select(Pet).options(sa.orm.selectinload(Pet.owner, Pet.vaccines, Pet.vaccines, Pet.blood_donations, Pet.blood_requests, Pet.blood_donation_responses, Pet.blood_request_responses))
+async def get_pets(db: AsyncSession) -> List[Pet]:
+    stmt = sa.select(Pet).options(
+        sa.orm.selectinload(Pet.owner),
+        sa.orm.selectinload(Pet.vaccines),
+        sa.orm.selectinload(Pet.blood_donations),
+        sa.orm.selectinload(Pet.blood_requests),
+        sa.orm.selectinload(Pet.blood_donation_responses),
+        sa.orm.selectinload(Pet.blood_request_responses),
+    )
     result = await db.execute(stmt)
-    pets = result.scalars().all()
-    print(pets)
-    return pets
+    return list(result.scalars().all())
